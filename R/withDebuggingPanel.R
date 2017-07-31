ca28d5301ea0fa5383ad430c2e1619ca792dd92e
Roger Day <notsofeo@gmail.com>
2017-07-31 12:24:40 -0400

    Another try at jquery-ui.js

--- R/withDebuggingPanel.R
+++ R/withDebuggingPanel.R
@@ -7,7 +7,7 @@ withDebuggingPanel = function() {
   list(
     #includeScript(paste0(path.package("shinyDebuggingPanel"),
      #                    "/debugPopup.js")),
-    includeScript("www/js/jquery-ui.js"),  ## (possibly needed)
+    includeScript("jquery-ui.js"),  ## (possibly needed)
     uiOutput("debugTools")
   )
 }
