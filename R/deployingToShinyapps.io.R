e235b28e5e5cc81e8cec1a574413384e4a8f7cef
Roger Day <notsofeo@gmail.com>
2017-07-31 12:09:28 -0400

    Moved the demo into inst/shinyDebuggingPanel. Better for .deploy.  Linking to deployingtoShinyapps.io.R, in DUE project; should work whenever there is just one file/dir in inst.

---
+++ R/deployingToShinyapps.io.R
@@ -0,0 +1,36 @@
+.installFromGithub =
+function(user='professorbeautiful', project=basename(getwd()), ...)
+  devtools::install_github(paste0(user, "/", project), ...)
+
+
+.deploy =
+function(app=dir('inst')[1], user='professorbeautiful', project=basename(getwd()),
+         reInstall=TRUE, ...){
+  ## TODO: first check that the html files are created committed and pushed.
+  packageWD = getwd()
+  if(reInstall)
+    .installFromGithub(user = user, project = project)
+  apps = app
+  for (app in apps) {
+    if(substr(app, 1, 5) == "inst/")
+      warning(".deploy: do not include 'inst' in app name.")
+    cat("wd is " , getwd(), "\n")
+    setwd(paste0("inst/", app))
+    cat("wd changing to ", getwd(), "\n")
+    tryCatch({
+      require("shinyapps")
+      deployApp(...)
+    },
+    finally={
+      cat("shinyapps::showLogs(appDir = 'inst/", app, "')\n")
+      setwd(packageWD)}
+    )
+  }
+}
+
+
+.runDeployed =
+function(app="shinyElicit"){
+  system("open https://trials.shinyapps.io/" %&% app)
+  cat("shinyapps::showLogs(appDir = 'inst/" %&% app %&% "')\n")
+}
