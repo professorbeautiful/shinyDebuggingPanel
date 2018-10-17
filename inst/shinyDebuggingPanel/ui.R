a3666d4d2a8361d4780306f863a4392d4c17392b
Roger Day <notsofeo@gmail.com>
2018-10-16 22:50:11 -0400

    Somewhat better example app.

--- inst/shinyDebuggingPanel/ui.R
+++ inst/shinyDebuggingPanel/ui.R
@@ -2,12 +2,29 @@
 
 shinyUI(
   navbarPage(
-    title="HELLO",
-    div(h1("EXAMPLE"),
+    title="shinyDebuggingPanel",
+    div(h1("A simple example"),
         hr(),
         shinyDebuggingPanel::withDebuggingPanel(),
-        'NEW debugTools!'
-        #                       uiOutput(outputId="debugTools"),
+        h4('To use this facility in your app:'),
+        pre(
+"
+- install the package with
+    install_github('professorbeautiful/shinyDebuggingPanel')
+- insert
+    require(shinyDebuggingPanel)
+into the global code for your app,
+- insert
+    withDebuggingPanel()
+into your UI,
+- insert
+      makeDebuggingPanelOutput(session, toolsInitialState = TRUE, condition='true')
+into your server code.",
+            br(),
+            pre(
+"Use the 'condition' arg to set up a toggle for visibility of the panel,
+for example driven by pressing a shortcut key.")
+        )
     )
   )
 )
