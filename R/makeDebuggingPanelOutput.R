41e67dc9ccbadd925e65af7d9b6d73d680cd87fc
Roger Day <notsofeo@gmail.com>
2021-08-05 15:52:51 -0400

    temporarily removing all observers.

--- R/makeDebuggingPanelOutput.R
+++ R/makeDebuggingPanelOutput.R
@@ -36,21 +36,21 @@ makeDebuggingPanelOutput = function(
       }
       assign('%&%',  function (a, b) paste(a, b, sep = ''))
       catn = function(...) cat(..., '\n')
-
-      observeEvent(input$evalButtonR, {
-        print("saw input$evalButtonR")
-        evalString = isolate(input$evalStringR)
-        rValuesDebugging_R$evalStringHistory =
-          c(rValuesDebugging_R$evalStringHistory, evalString)
-        cat("length evalStringHistory = ",
-            length(rValuesDebugging_R$evalStringHistory),  '\n')
-        rValuesDebugging_R$capturedOutput =
-          capture.output(try(eval(parse(text=evalString))))
-        updateNumericInput(session = thisSession, inputId = 'idRlineNum',
-                           value = length(rValuesDebugging_R$evalStringHistory),
-                           max = length(rValuesDebugging_R$evalStringHistory))
-      })
-
+#
+#       observeEvent(input$evalButtonR, {
+#         print("saw input$evalButtonR")
+#         evalString = isolate(input$evalStringR)
+#         rValuesDebugging_R$evalStringHistory =
+#           c(rValuesDebugging_R$evalStringHistory, evalString)
+#         cat("length evalStringHistory = ",
+#             length(rValuesDebugging_R$evalStringHistory),  '\n')
+#         rValuesDebugging_R$capturedOutput =
+#           capture.output(try(eval(parse(text=evalString))))
+#         updateNumericInput(label = ' ', session = thisSession, inputId = 'idRlineNum',
+#                            value = length(rValuesDebugging_R$evalStringHistory),
+#                            max = length(rValuesDebugging_R$evalStringHistory))
+#       })
+#
       output$evaluatedOutputR = renderUI({
           HTML(paste(collapse='<br>', rValuesDebugging_R$capturedOutput))
           #capturedOutput
@@ -61,45 +61,45 @@ makeDebuggingPanelOutput = function(
       # EXAMPLE:  window.Shiny.shinyapp.$bindings.selTxt.firstChild.nodeValue
       inputPreambleJS <<- 'window.Shiny.shinyapp.$inputValues.'
       wrapperToGetKeys <<- function(x) 'Object.keys(' %&% x %&% ')'
-
-      observeEvent(input$idJSlineNum, {
-        print(input$idJSlineNum)
-        if(!is.na(input$idJSlineNum) & (input$idJSlineNum > 0) &
-           input$idJSlineNum <= length(rValuesDebugging_JS$evalStringHistory)) {
-          print(rValuesDebugging_JS$evalStringHistory[[input$idJSlineNum]])
-          updateTextAreaInput(session = thisSession, inputId = 'evalStringJS',
-                              value = rValuesDebugging_JS$evalStringHistory
-                              [[input$idJSlineNum]])
-        }
-      })
-      observerPreambleToggles = observe({
-        input$prependInputPreambleToggle
-        input$prependOutputPreambleToggle
-        try({
-          evalString = isolate(input$evalStringJS)
-          if(wasClicked(input$prependInputPreambleToggle)) {
-            if(substr(evalString, 1, nchar(inputPreambleJS)) != inputPreambleJS)
-              evalString = paste0(inputPreambleJS, evalString)
-          }
-          else ## Remove inputPreambleJS
-            evalString = gsub(inputPreambleJS, '', evalString, fixed=TRUE)
-
-          if(wasClicked(input$prependOutputPreambleToggle)) {
-            if(substr(evalString, 1, nchar(outputPreambleJS)) != outputPreambleJS)
-              evalString = paste0(outputPreambleJS, evalString)
-          }
-          else ## Remove outputPreambleJS
-            evalString = gsub(outputPreambleJS, '', evalString, fixed=TRUE)
-
-          isolate( { rValuesDebugging_JS$evalStringJS = evalString } )
-          catn('Responding to preamble toggles, evalString=', evalString)
-          updateTextInput(thisSession, 'evalStringJS',
-                          label='',
-                          value=rValuesDebugging_JS$evalStringJS)
-          # You need to specify the label arg too. The default, NULL, doesn't cut it.
-        })
-      })
-
+#
+#       observeEvent(input$idJSlineNum, {
+#         print(input$idJSlineNum)
+#         if(!is.na(input$idJSlineNum) & (input$idJSlineNum > 0) &
+#            input$idJSlineNum <= length(rValuesDebugging_JS$evalStringHistory)) {
+#           print(rValuesDebugging_JS$evalStringHistory[[input$idJSlineNum]])
+#           updateTextAreaInput(label=' ', session = thisSession, inputId = 'evalStringJS',
+#                               value = rValuesDebugging_JS$evalStringHistory
+#                               [[input$idJSlineNum]])
+#         }
+#       })
+#       observerPreambleToggles = observe({
+#         input$prependInputPreambleToggle
+#         input$prependOutputPreambleToggle
+#         try({
+#           evalString = isolate(input$evalStringJS)
+#           if(wasClicked(input$prependInputPreambleToggle)) {
+#             if(substr(evalString, 1, nchar(inputPreambleJS)) != inputPreambleJS)
+#               evalString = paste0(inputPreambleJS, evalString)
+#           }
+#           else ## Remove inputPreambleJS
+#             evalString = gsub(inputPreambleJS, '', evalString, fixed=TRUE)
+#
+#           if(wasClicked(input$prependOutputPreambleToggle)) {
+#             if(substr(evalString, 1, nchar(outputPreambleJS)) != outputPreambleJS)
+#               evalString = paste0(outputPreambleJS, evalString)
+#           }
+#           else ## Remove outputPreambleJS
+#             evalString = gsub(outputPreambleJS, '', evalString, fixed=TRUE)
+#
+#           isolate( { rValuesDebugging_JS$evalStringJS = evalString } )
+#           catn('Responding to preamble toggles, evalString=', evalString)
+#           updateTextInput(thisSession, 'evalStringJS',
+#                           label=' ',
+#                           value=rValuesDebugging_JS$evalStringJS)
+#           # You need to specify the label arg too. The default, NULL, doesn't cut it.
+#         })
+#       })
+#
       #output$evaluatedOutputJS = renderText({
       #shinyalert('JS output is in a popup alert window, if there was no error. Otherwise nothing happens')
       # }
@@ -114,7 +114,7 @@ makeDebuggingPanelOutput = function(
               c(rValuesDebugging_JS$evalStringHistory, evalString);
             cat('length(rValuesDebugging_JS$evalStringHistory)\n');
             print(length(rValuesDebugging_JS$evalStringHistory));
-            updateNumericInput(session = thisSession, inputId = 'idJSlineNum',
+            updateNumericInput(label=' ', session = thisSession, inputId = 'idJSlineNum',
                                value = length(rValuesDebugging_JS$evalStringHistory),
                                max = length(rValuesDebugging_JS$evalStringHistory))
           })
@@ -141,17 +141,19 @@ makeDebuggingPanelOutput = function(
               ifelse(input$traceCheckbox, 'on', 'off'))
       })   #### OK this works now.
 
-      observeEvent(input$idRlineNum, {
-        print(input$idRlineNum)
-        if(!is.na(input$idRlineNum) & (input$idRlineNum > 0) &
-           input$idRlineNum <= length(rValuesDebugging_R$evalStringHistory)) {
-          print(rValuesDebugging_R$evalStringHistory[[input$idRlineNum]])
-          updateTextAreaInput(session = thisSession, inputId = 'evalStringR',
-                              value = rValuesDebugging_R$evalStringHistory
-                              [[input$idRlineNum]])
-        }
-      })
-      output$debugTools = renderUI({
+      # observeEvent(input$idRlineNum, {
+      #   print(input$idRlineNum)
+      #   if(!is.na(input$idRlineNum) & (input$idRlineNum > 0) &
+      #      input$idRlineNum <= length(rValuesDebugging_R$evalStringHistory)) {
+      #     print(rValuesDebugging_R$evalStringHistory[[input$idRlineNum]])
+      #     updateTextAreaInput(label=' ', session = thisSession, inputId = 'evalStringR',
+      #                         value = rValuesDebugging_R$evalStringHistory
+      #                         [[input$idRlineNum]])
+      #   }
+      # })
+      #
+      output$debugTools =
+        renderUI({
         shiny::conditionalPanel(
           condition=theShowDebuggerCondition,
           div(style="background:darkGrey",
@@ -165,70 +167,71 @@ makeDebuggingPanelOutput = function(
                         ),
                         column(4, radioButtons('id_languageChoice', '',
                                                choices=c('R', 'JS'),
-                                               inline=TRUE)
+                                               inline=TRUE, label='XYZ')
                                # inputId='debugToolsCheckbox_JS', value=toolsInitialState,
                                # label=em(strong("Debug a shiny apps: evaluate R and JS")))
                         ),
                         column(4, checkboxInput(
                           inputId="traceCheckbox",
-                          value=FALSE,
+                          value=TRUE,
                           label=em(strong(textOutput("shiny.trace.text")))
                         )
                         )
-              ),
-              conditionalPanel(
-                'input.debugToolsCheckbox',
-                conditionalPanel(
-                  'input.id_languageChoice==="R"',
-                  fluidRow(
-                    column(2, offset = 1,
-                           actionButton(
-                             "evalButtonR",
-                             HTML(
-                               "<font color='dark red' style='font-weight:bold'> evaluate R</font>")),
-                           numericInput('idRlineNum',  label = "command\nhistory",
-                                        width = '100px',
-                                        value = 1, min=1)
-                    ),
-                    column(9,
-                           tagAppendAttributes(
-                             style="width:550px; height:150px;",
-                             tags$textarea(
-                               id = "evalStringR", label="R code",
-                               value="1234"))
-                    ),
-                    tagAppendAttributes(width=800,
-                                        style='text-align:"right"; color:green',
-                                        bsModal(id = 'evaluateR_popup',
-                                                title="Output of R command",
-                                                trigger = "evalButtonR",
-                                                size="large",
-                                                uiOutput(outputId="evaluatedOutputR")))
-                  )),
-                conditionalPanel(
-                  'input.id_languageChoice==="JS"',
-                  fluidRow(
-                    column(2, offset = 1,
-                           actionButton( inputId = "evalButtonJS",
-                            label = HTML(
-                               "<font color='dark red' style='font-weight:bold'> evaluate JS</font>")),
-                           numericInput('idJSlineNum', label = "command\nhistory",
-                                        width = '100px', value = 1, min=1)
-                           ),
-                    column(9, tagAppendAttributes(
-                      style="width:550px; height:150px;",
-                      tags$textarea(id = "evalStringJS",
-                                    value="") ),
-                           checkboxInput(inputId="prependOutputPreambleToggle",
-                                         value=FALSE,
-                                         label="prepend/remove Output Preamble"),
-                           checkboxInput(inputId="prependInputPreambleToggle",
-                                         value=FALSE,
-                                         label="prepend/remove Input Preamble")
-                    )
-                  )
-                )
-              ),
+              )
+              ,
+              # conditionalPanel(
+              #   'input.debugToolsCheckbox',
+              #   conditionalPanel(
+              #     'input.id_languageChoice==="R"',
+              #     fluidRow(
+              #       column(2, offset = 1,
+              #              actionButton(
+              #                "evalButtonR",
+              #                HTML(
+              #                  "<font color='dark red' style='font-weight:bold'> evaluate R</font>")),
+              #              numericInput('idRlineNum',  label = "command\nhistory",
+              #                           width = '100px',
+              #                           value = 1, min=1)
+              #       ),
+              #       column(9,
+              #              tagAppendAttributes(
+              #                style="width:550px; height:150px;",
+              #                tags$textarea(
+              #                  id = "evalStringR", label="R code",
+              #                  value="1234"))
+              #       ),
+              #       tagAppendAttributes(width=800,
+              #                           style='text-align:"right"; color:green',
+              #                           bsModal(id = 'evaluateR_popup',
+              #                                   title="Output of R command",
+              #                                   trigger = "evalButtonR",
+              #                                   size="large",
+              #                                   uiOutput(outputId="evaluatedOutputR")))
+              #     )),
+              #   conditionalPanel(
+              #     'input.id_languageChoice==="JS"',
+              #     fluidRow(
+              #       column(2, offset = 1,
+              #              actionButton( inputId = "evalButtonJS",
+              #               label = HTML(
+              #                  "<font color='dark red' style='font-weight:bold'> evaluate JS</font>")),
+              #              numericInput('idJSlineNum', label = "command\nhistory",
+              #                           width = '100px', value = 1, min=1)
+              #              ),
+              #       column(9, tagAppendAttributes(
+              #         style="width:550px; height:150px;",
+              #         tags$textarea(id = "evalStringJS",
+              #                       value="") ),
+              #              checkboxInput(inputId="prependOutputPreambleToggle",
+              #                            value=FALSE,
+              #                            label="prepend/remove Output Preamble"),
+              #              checkboxInput(inputId="prependInputPreambleToggle",
+              #                            value=FALSE,
+              #                            label="prepend/remove Input Preamble")
+              #       )
+              #     )
+              #   )
+              # ),
               #list(HTML(paste0(rep("&nbsp;",15), collapse=""))),
               uiOutput(outputId='JSevaluation')
           )
