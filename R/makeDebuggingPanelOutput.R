5eb581867cd3ff6e29ee999804330a1e5ddd6aff
Roger Day <notsofeo@gmail.com>
2021-08-06 22:46:16 -0400

    fluidRow_R by itself is ok.

--- R/makeDebuggingPanelOutput.R
+++ R/makeDebuggingPanelOutput.R
@@ -228,7 +228,7 @@ makeDebuggingPanelOutput = function(
               ,
               conditionalPanel(
                 'input.debugToolsCheckbox',
-                ' ',fluidRow_R, fluidRow_JS
+                ' ',fluidRow_R
                 # conditionalPanel(
                 #   'input.id_languageChoice=="R"',
                 #   fluidRow_R),
